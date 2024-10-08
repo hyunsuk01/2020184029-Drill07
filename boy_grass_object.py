1baead02dd93ecc3c3c35a6b2004161c55ee528c
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2024-10-08 12:47:56 +0900

    랜덤한 위치에서 우측으로 이동하는 캐릭터(클래스, 객체 사용)

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,34 @@
 from pico2d import *
 
+import random
+
 # Game object class here
+class Grass:
+    # 생성자를 이용해서 객체의 초기 상태를 정의
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 400), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -11,11 +39,40 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def update_world():
+    grass.update()
+    for boy in team:
+        boy.update()
+
+    pass
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
+def reset_world(): # 초기화하는 함수
+    global running
+    global grass
+    global team
+
+    running = True
+    grass = Grass() # Grass 클래스를 이용해서 grass 객체 생성
+    team = [Boy() for i in range(11)]
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
 
 # finalization code
 
